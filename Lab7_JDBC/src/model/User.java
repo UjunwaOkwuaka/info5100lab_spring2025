8425a00ddee062fb2c4e56b59b93abeee1340374
Ujunwa Okwuaka <okwuaka.u@northeastern.edu>
2025-03-19 15:29:25 -0400

    Lab 7 JDBC

---
+++ Lab7_JDBC/src/model/User.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+
+
+/**
+ *
+ * @author okwua
+ */
+public class User {
+
+    //data members
+    private int userId;
+    private String name;
+    private String college;
+    private int age;
+    private String hobby;
+    
+
+    
+    //methods
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    @Override
+    public String toString() {
+        return "User Profile Information:\n" + "userId= " + userId + "\n" + "name= " + name + "\n" +  "college= " + college + "\n" + "age= " + age + "\n" + " hobby= " + hobby + '}';
+    }
+}
+
+    
+
+    
